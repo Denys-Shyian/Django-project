2d455a098c5d5b98d0e7bede5e3dbfab2eacde75
Denys <xDen4ik@icloud.com>
2022-04-29 00:07:02 +0300

    add new function

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,5 @@
+import string
+
 from django.shortcuts import render
 
 
@@ -8,4 +10,7 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_test = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_test})
+    number_of_words = sum([i.strip(string.punctuation).isalpha() for i in user_text.split()])
+
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_test,
+                                            'numberofwords': number_of_words})
